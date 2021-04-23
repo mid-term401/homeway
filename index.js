ff0003e89300288324e09dd467db0d1d8aefd3d3
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2021-04-23 14:27:10 +0300

    Sign up

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 const superagent = require("superagent");
 const pg = require("pg");
+const path = require("path");
 
 // Esoteric Resources
 // const v1Routes = require('../api-server/src/routes/v1');
@@ -14,7 +15,16 @@ const notFound = require("./src/error-handlers/404.js");
 
 // Prepare the express app
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
 const PORT = process.env.PORT || 9000;
+
 
 const client = new pg.Client(process.env.DATABASE_URL);
 // const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
@@ -23,6 +33,43 @@ const client = new pg.Client(process.env.DATABASE_URL);
 app.use(cors());
 
 // Routes
+app.get("/", handleHome );
+
+app.get("/sign_up", handleSignUpForm );
+
+app.post("/sign_up", handleSignup)
+
+app.get("/sign_in", handleSignIn );
+
+// functions
+
+function handleHome(req, res) {
+  res.render("index.html");
+}
+
+function handleSignUpForm(req, res) {
+  res.render("signup.html");
+}
+
+function handleSignup(req, res) {
+  const formData = req.body;
+
+  let insertQuery = "insert into <name of the table>(user_name, first_name, last_name, password, email, country, birth_date, address) values ($1, $2, $3, $4, $5, $6, $7, $8)"
+  const safeValues = [formData.username, formData.password, formData.first_name, formData.last_name, formData.password, formData.email, formData.country, formData.address];
+
+  client.query(insertQuery, safeValues)
+      .then(data => {
+        console.log(`Data added to the database`);
+        res.redirect("/");
+      })
+      .catch(error => {
+      console.log('', error)
+      })
+}
+
+function handleSignIn() {
+
+}
 
 // Catchalls
 app.use(notFound);
