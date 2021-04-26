2767514e48acda3807d9dbdf3fc89e5e6f5da161
haneen <haneen.khasawneh96@gmail.com>
2021-04-26 05:39:59 +0300

    AOuth Complete

--- src/server.js
+++ src/server.js
@@ -79,13 +79,88 @@ app.use(cors());
 
 //AOuth 
 const {OAuth2Client} = require('google-auth-library');
-const Gclient = new OAuth2Client(process.env.CLIENT_ID);
+const CLIENT_ID = '828937553057-8gc5eli5vu3v2oig6rphup580sg33lj4.apps.googleusercontent.com'
+const Gclient = new OAuth2Client(CLIENT_ID);
+
+
+// app.set('view engine', 'ejs');
+// // app.use(express.json());
+// // app.use(cookieParser());
+// app.use(express.static('public'));
+
+
+// Middleware
 
 app.get('/', (req, res)=>{
   res.render('index')
 })
 
+app.get('/login', (req,res)=>{
+  res.render('login');
+})
 
+app.post('/login', (req,res)=>{
+  let token = req.body.token;
+
+  async function verify() {
+      const ticket = await Gclient.verifyIdToken({
+          idToken: token,
+          audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      });
+      const payload = ticket.getPayload();
+      const userid = payload['sub'];
+    }
+    verify()
+    .then(()=>{
+        res.cookie('session-token', token);
+        res.send('success')
+    })
+    .catch(console.error);
+
+})
+
+app.get('/profile', checkAuthenticated, (req, res)=>{
+  let user = req.user;
+  res.render('profile', {user});
+})
+
+app.get('/protectedRoute', checkAuthenticated, (req,res)=>{
+  res.send('This route is protected')
+})
+
+app.get('/logout', (req, res)=>{
+  res.clearCookie('session-token');
+  res.redirect('/login')
+
+})
+
+
+function checkAuthenticated(req, res, next){
+
+  let token = req.cookies['session-token'];
+  console.log(token);
+
+  let user = {};
+  async function verify() {
+      const ticket = await Gclient.verifyIdToken({
+          idToken: token,
+          audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      });
+      const payload = ticket.getPayload();
+      user.name = payload.name;
+      user.email = payload.email;
+      user.picture = payload.picture;
+    }
+    verify()
+    .then(()=>{
+        req.user = user;
+        next();
+    })
+    .catch(err=>{
+        res.redirect('/login')
+    })
+
+}
 
 ////////////////////////////////////
 // Routes
