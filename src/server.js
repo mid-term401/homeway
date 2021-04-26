81763440bfebbd8e1dd718f20e4eb07404fa10f6
Samer-Alnajjar <samerk.alnajjar@gmail.com>
2021-04-26 03:42:47 +0300

    testing Oauth

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@
 const express = require("express");
 
 const client = require("../DataBase/data")
+console.log(client);
 
 const cors = require("cors");
 require("dotenv").config();
@@ -76,6 +77,90 @@ const {  handleSearchBar,
 
 // App Level MW
 app.use(cors());
+
+// Oauth
+
+// const {OAuth2Client} = require('google-auth-library');
+// const CLIENT_ID = '967817971714-qeu311a41ou2mv7q88j69613eeklg24g.apps.googleusercontent.com'
+// const googleClient = new OAuth2Client(CLIENT_ID);
+
+// // Middleware
+
+// // app.set('view engine', 'ejs');
+// // app.use(express.json());
+// // app.use(cookieParser());
+// // app.use(express.static('public'));
+
+// app.get('/', (req, res)=>{
+//     res.render('index')
+// })
+
+// app.get('/login', (req,res)=>{
+//     res.render('login');
+// })
+
+// app.post('/login', (req,res)=>{
+//     let token = req.body.token;
+
+//     async function verify() {
+//         const ticket = await googleClient.verifyIdToken({
+//             idToken: token,
+//             audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+//         });
+//         const payload = ticket.getPayload();
+//         const userid = payload['sub'];
+//       }
+//       verify()
+//       .then(()=>{
+//           res.cookie('session-token', token);
+//           res.send('success')
+//       })
+//       .catch(console.error);
+
+// })
+
+// app.get('/profile', checkAuthenticated, (req, res)=>{
+//     let user = req.user;
+//     res.render('profile', {user});
+// })
+
+// app.get('/protectedRoute', checkAuthenticated, (req,res)=>{
+//     res.send('This route is protected')
+// })
+
+// app.get('/logout', (req, res)=>{
+//     res.clearCookie('session-token');
+//     res.redirect('/login')
+
+// })
+
+
+// function checkAuthenticated(req, res, next){
+
+//     let token = req.cookies['session-token'];
+
+//     let user = {};
+//     async function verify() {
+//         const ticket = await googleClient.verifyIdToken({
+//             idToken: token,
+//             audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+//         });
+//         const payload = ticket.getPayload();
+//         user.name = payload.name;
+//         user.email = payload.email;
+//         user.picture = payload.picture;
+//       }
+//       verify()
+//       .then(()=>{
+//           req.user = user;
+//           next();
+//       })
+//       .catch(err=>{
+//           res.redirect('/login')
+//       })
+
+// }
+
 
 // Routes
 app.get("/volunteer/:id", bearerVolunteer, handleGetVolunteerProfile);
