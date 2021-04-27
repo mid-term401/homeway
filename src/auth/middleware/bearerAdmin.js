9945225711604a93efb0c854479c12924ec813f6
Boshra Jaber <boshrajaber94@gmail.com>
2021-04-27 04:56:54 +0300

    bearer tests

--- src/auth/middleware/bearerAdmin.js
+++ src/auth/middleware/bearerAdmin.js
@@ -20,7 +20,8 @@ module.exports = async (req, res, next) => {
       next();
     }
   } catch (e) {
-    res.json("Invalid Login");
+    // res.json("Invalid Login");
+    res.status(403)
   }
 
 }
